155713a40b1e332b08f5b6f1b9e6f21bf3c9ccf2
SSR707 <samandarshavkatov07@gmail.com>
2025-03-07 14:40:58 +0500

    add

--- src/api/debtors/dto/create-debtor.dto.ts
+++ src/api/debtors/dto/create-debtor.dto.ts
@@ -28,7 +28,7 @@ export class CreateDebtorDto {
   note?: string;
 
   @ApiProperty({ example: '123e4567-e89b-12d3-a456-426614174000' })
-  @IsNotEmpty()
+  @IsOptional()
   @IsUUID()
   store_id: string;
 }
